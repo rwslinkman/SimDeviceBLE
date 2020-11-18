ea1a2cd9daafc1f2f4a68e56d677e81aeefd4f75
Rick Slinkman <rw.slinkman@gmail.com>
2020-11-18 21:59:49 +0100

    Created empty activity for gRPC interactions.

---
+++ app/src/main/java/nl/rwslinkman/simdeviceble/grpc/GrpcServerActivity.kt
@@ -0,0 +1,12 @@
+package nl.rwslinkman.simdeviceble.grpc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nl.rwslinkman.simdeviceble.R
+
+class GrpcServerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grpc_server)
+    }
+}
