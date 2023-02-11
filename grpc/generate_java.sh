b71b5b94dc6137fbaf7f8ebb8ae670f7ed5401bf
Rick Slinkman <rw.slinkman@gmail.com>
2023-02-11 22:50:54 +0100

    Fixed code generation for gRPC server.

--- grpc/generate_java.sh
+++ grpc/generate_java.sh
@@ -5,14 +5,15 @@
 print "SimDeviceBLE gRPC code generator"
 print "Cleaning up generated code..."
 rm -rf ./grpc/generated_java
+rm -rf ./grpc/generated_kt
 mkdir ./grpc/generated_java
 
 print "Running code generator..."
-protoc -I=./ --java_out=./grpc/generated_java ./grpc/SimDeviceBLE.proto
+protoc -I=./grpc --plugin=protoc-gen-grpc-java=./grpc/compiler/protoc-gen-grpc-java --java_out=./grpc/generated_java --grpc-java_out=./grpc/generated_java ./grpc/SimDeviceBLE.proto
 print "Code generation completed!"
 
 print "Applying generated code to project source"
 rm -rf app/build/generated/src
-mkdir -p app/build/generated/src/nl/rwslinkman/simdeviceble/grpc/server/
-cp grpc/generated_java/nl/rwslinkman/simdeviceble/grpc/server/* app/build/generated/src/nl/rwslinkman/simdeviceble/grpc/server/
+mkdir -p ./app/build/generated/src/nl/rwslinkman/simdeviceble/grpc/server/
+cp ./grpc/generated_java/nl/rwslinkman/simdeviceble/grpc/server/* app/build/generated/src/nl/rwslinkman/simdeviceble/grpc/server/
 print "Copied files to 'app/build/generated/src' directory!"
